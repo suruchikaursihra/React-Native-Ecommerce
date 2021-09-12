4c574c901ee738ce90d999940a8bfd490c172c7c
Suruchi Kaur <suruchikaur@Administrators-MacBook-Air.local>
2021-09-12 21:44:40 +0530

    theme aaded and navigation

--- android/app/src/main/java/com/rn_project/MainActivity.java
+++ android/app/src/main/java/com/rn_project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rn_project;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +13,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "RN_Project";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
+
 }
